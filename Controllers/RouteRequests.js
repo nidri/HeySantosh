d7eadfb395c37c04e5fbadc6d6ae0a6d5d3da2d8
Krishna Santosh Nidri <krishna.nidri@gmail.com>
2018-01-28 18:52:59 -0500

    Changed HandleIncomingRequest to promises

--- Controllers/RouteRequests.js
+++ Controllers/RouteRequests.js
@@ -3,54 +3,63 @@ var https = require('https');
 var fs = require('fs');
 var url = require('url');
 
-function HandleCSSAndJSRequest(Request, Response) {
-  // Handle CSS requests
-  if(!Request.url.toLowerCase().endsWith("css") && !Request.url.toLowerCase().endsWith("js")) {
-    console.log("Not processing - " + Request.url);
-    return Response.end();
-  }
-  else {
-    console.log("Processing - " + Request.url);
-    fs.readFile("." + Request.url, function(Err, Stream) {
-      if(!Err) {
-        var ContentType = "";
-        if(Request.url.toLowerCase().endsWith("css")) ContentType = "text/css";
-        else ContentType = "application/javascript";
-        Response.statusCode = "200";
-        Response.setHeader("Content-Type", ContentType);
-        Response.write(Stream);
-        Response.end();
+function FetchResponse(URL) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(URL, function(Error, Stream) {
+      if(!Error) {
+        resolve(Stream);
       }
+      else reject(Error);
+    });
   });
+} // End of FetchResponse
+
+function GetContentType(URL) {
+  // Return content type
+  var ContentType = "html";
+  if(URL.toLowerCase().endsWith("css")) {
+    ContentType = "css";
   }
-}
+  else if (URL.toLowerCase().endsWith("js")) {
+    ContentType = "js";
+  }
+  else if(URL.toLowerCase().endsWith("jpeg") || URL.toLowerCase().endsWith("jpg")) {
+    ContentType = "jpeg";
+  }
+  return ContentType;
+} // End of GetContentType
+
 function HandleIncomingRequest(Request, Response) {
   return new Promise(function(resolve, reject) {
-    console.log("Handling request - " + Request.url);
-    // If main page then respond with root index.html
-    if(Request.url == "/") {
-        fs.readFile("./Views/Html/Index.html", function(Err, Stream) {
-          if(!Err) {
-            // Respond with stream
-            Response.statusCode = "200";
-            Response.write(Stream);
-          }
-          else {
-            Response.statusCode = "404";
-            // Respond with "page not found html"
-          }
-          Response.end();
-        });
-        return resolve(Response);
-      }
-    else {
-      HandleCSSAndJSRequest(Request, Response)
-      return resolve(Response);
+    var ContentTypes = {
+      "css": "text/css",
+      "js": "application/javascript",
+      "html": "text/html",
+      "jpeg": "image/jpeg"
     };
-    //resolve(Response);
-  });
+    var Path = {
+      "/": "/Views/Html/Index.html"
+    };
+    var URL = ".";
+    URL = URL + (Path[Request.url] ? Path[Request.url] : Request.url);
+    var ContentType = ContentTypes[GetContentType(URL)];
+    console.log("Processing request - " + URL);
+    FetchResponse(URL)
+    .then(function(Data) {
+      Response.statusCode = "200";
+      Response.setHeader = ("Content-Type", ContentType);
+      Response.write(Data);
+      Response.end();
+      resolve(Response);
+    })
+    .catch(function(Error) {
+      console.log("Error occured while processing - " + URL + " - " + Error);
+      Response.end();
+      reject(Response);
+    });
+  }); // End of FetchResponse
+} // End of HandleIncomingRequest
 
-}
 module.exports = {
   "HandleIncomingRequest": HandleIncomingRequest
 };
